c78c02c97de5d34d6c9d4e75e7051b5d161a745d
seoyeon08 <silversun0808@gmail.com>
2023-04-09 21:33:31 +0900

    Modify check.py less data

--- check.py
+++ check.py
@@ -4,8 +4,8 @@ with open('data/train.json', 'r') as f:
     train_data = json.load(f) 
 train_data = [item for topic in train_data['data'] for item in topic['paragraphs'] ]
 
-# 넘 많으니까 1000개만 데이터 사용하기
-using_num_sample = 1000
+# 넘 많으니까 100개만 데이터 사용하기
+using_num_sample = 100
 train_data = train_data[:using_num_sample]
 
 print(train_data)
